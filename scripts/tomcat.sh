7682d8908953a517ac3ff018f83270048896028d
Timur Eshpulatov <timur.eshpulatov@gmail.com>
2020-08-07 17:11:56 +0300

    Added check tomcat wars status

--- scripts/tomcat.sh
+++ scripts/tomcat.sh
@@ -3,7 +3,7 @@
 TOMCAT_USER=tomcatadmin
 TOMCAT_PASSWORD=tomcatpassword
 
-
+if [ -z $1 ];then
 es=''
 for i in $(/usr/bin/curl -ss --max-time 5 -u $TOMCAT_USER:$TOMCAT_PASSWORD "http://localhost:8080/manager/text/list" | grep '^/' | awk -F: '{print $1}' | sed 's/\///g')
 do
@@ -12,3 +12,12 @@ es=","
 done
 
 echo -e "{\"data\":[$OutStr]}"
+exit
+fi
+
+
+if [ -n $1 ];then
+/usr/bin/curl -ss --max-time 5 -u tomcatadmin:5TYjwCDgZfHYPDbK "http://localhost:8080/manager/text/list" | grep $1: | grep running &>/dev/null ; if [ $? -eq 0 ];then echo 1 ; else echo 0; fi
+exit
+fi
+
